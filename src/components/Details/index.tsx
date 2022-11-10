d91408fdc582246e6ccf7674af246df430f67998
Breno Cocheto <48560880+bcocheto@users.noreply.github.com>
2022-11-10 13:15:35 -0300

    feat: Modal de detalhes

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import { Button, Image, Modal, Text, TouchableWithoutFeedback, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, Modal, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
 import { Item } from '../../types/Item';
 import { styles } from './style';
+import { AntDesign } from '@expo/vector-icons';
 import * as Animatable from 'react-native-animatable';
 
 interface DetailsProps {
@@ -11,7 +12,21 @@ interface DetailsProps {
 }
 
 export const DetailsComponent = ({ isOpen, toggleModal, selectedItem }: DetailsProps) => {
+  const [isEditIcon, setIsEditIcon] = useState(true);
+  const [deleteItem, setDeleteItem] = useState('');
 
+  useEffect(() => {
+    setIsEditIcon(true)
+    setDeleteItem('');
+  }, [isOpen]);
+
+  const handleEdit = () => {
+    setIsEditIcon((prev)=>!prev)
+  }
+
+  const handleDelete = () => {
+    setDeleteItem('jello');
+  }
 
 
   return (
@@ -20,6 +35,27 @@ export const DetailsComponent = ({ isOpen, toggleModal, selectedItem }: DetailsP
         <View style={styles.modal}>
           <Animatable.View animation={'slideInUp'} style={styles.modalContentWrapper}>
             <View style={styles.modalHeader}>
+
+              <View style={styles.modalConfig}>
+                <TouchableWithoutFeedback onPress={handleEdit}>
+                  {isEditIcon ?
+                    (
+                      <Animatable.View delay={isEditIcon ? 0 : 1000} animation={isEditIcon ? '' :'zoomOut'}>
+                        <AntDesign name="edit" size={24} style={styles.icon} />
+                      </Animatable.View>
+                    ) : (
+                      <Animatable.View delay={1000} animation={'zoomIn'}>
+                        <AntDesign name="check" size={24} style={styles.icon} />
+                      </Animatable.View>
+                    )
+                  }
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={handleDelete}>
+                  <Animatable.View animation={deleteItem}>
+                  <AntDesign name="delete" size={24} style={styles.icon} />
+                  </Animatable.View>
+                </TouchableWithoutFeedback>
+              </View>
 
               <View style={styles.modalHeaderContent}>
                 <Image
@@ -30,10 +66,40 @@ export const DetailsComponent = ({ isOpen, toggleModal, selectedItem }: DetailsP
               </View>
 
             </View>
-            <View>
-              <Text>{selectedItem.quantity}</Text>
-              <Text>{selectedItem.category}</Text>
-              <Text>{selectedItem.description}</Text>
+
+            <View style={styles.modalContent}>
+              {!isEditIcon ? (
+              <View>
+                <Text style={styles.secondary_title}>QUANTIDADE</Text>
+                <Text numberOfLines={1} style={styles.subTitle}>{selectedItem.quantity}</Text>
+                <Text style={styles.secondary_title}>CATEGORIA</Text>
+                <Text numberOfLines={1} style={styles.subTitle}>{selectedItem.category}</Text>
+                <Text style={styles.secondary_title}>DESCRIÇÃO</Text>
+                <Text numberOfLines={3} style={styles.subTitle}>{selectedItem.description}</Text>
+              </View>
+            ):(
+              <View>
+                <Text style={styles.secondary_title}>QUANTIDADE</Text>
+                    <TextInput
+                      numberOfLines={1}
+                      keyboardType='numeric'
+                      style={styles.subTitle}
+                      placeholder={String(selectedItem.quantity)}
+                    />
+                <Text style={styles.secondary_title}>CATEGORIA</Text>
+                    <TextInput
+                      numberOfLines={1}
+                      style={styles.subTitle}
+                      placeholder={selectedItem.category}
+                    />
+                <Text style={styles.secondary_title}>DESCRIÇÃO</Text>
+                    <TextInput
+                      numberOfLines={3}
+                      style={styles.subTitle}
+                      placeholder={selectedItem.description}
+                    />
+              </View>
+            )}
             </View>
           </Animatable.View>
         </View>
--- src/components/Details/style.ts
+++ src/components/Details/style.ts
@@ -1,5 +1,8 @@
 import { StyleSheet } from "react-native";
 import { COLORS_ENUM } from "../../enums/ColorsEnum";
+import { SHADOWS_RADIUS_ENUM } from "../../enums/ShadowRadius";
+import { SHADOWS_OFFSET_ENUM } from "../../enums/ShadowsOffset";
+import { SHADOWS_OPACITY_ENUM } from "../../enums/ShadowsOpacity";
 
 export const styles = StyleSheet.create({
   modal: {
@@ -8,16 +11,14 @@ export const styles = StyleSheet.create({
   },
   modalContentWrapper: {
     flex: 1,
-    top: '55%',
+    top: '45%',
     backgroundColor: 'white',
     margin: 16,
     borderRadius: 50,
     elevation:30
   },
   modalHeader: {
-    flexDirection:'row',
-    justifyContent: 'space-evenly',
-    alignItems:'center',
+    flexDirection:'column',
     backgroundColor: COLORS_ENUM.primary,
     padding: 16,
     borderTopLeftRadius: 50,
@@ -27,6 +28,26 @@ export const styles = StyleSheet.create({
     flexDirection:'column',
     justifyContent: 'center',
     alignItems:'center',
+  },
+  modalContent: {
+    flex: 1,
+    padding:16,
+  },
+  modalConfig: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  icon: {
+    margin: 6,
+    color: COLORS_ENUM.primary_light,
+    shadowColor:"#000",
+    shadowOffset: {
+      width: SHADOWS_OFFSET_ENUM.basic_width,
+      height:SHADOWS_OFFSET_ENUM.basic_height
+    },
+    shadowOpacity: SHADOWS_OPACITY_ENUM.basic,
+    shadowRadius: SHADOWS_RADIUS_ENUM.basic,
+    elevation:6,
   },
   image:{
     borderRadius: 100 * .2,
@@ -45,4 +66,19 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: COLORS_ENUM.primary_light,
   },
+  secondary_title: {
+    fontSize: 14,
+    fontWeight:'500',
+    lineHeight: 18,
+    letterSpacing:-.5,
+    color: COLORS_ENUM.primary_text,
+  },
+  subTitle: {
+    fontSize: 14,
+    fontWeight:'400',
+    lineHeight: 16,
+    letterSpacing:-.2,
+    color: COLORS_ENUM.secondary_text,
+    marginBottom:5
+  }
 })
